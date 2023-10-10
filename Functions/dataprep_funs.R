4c6839aca12c1c579ca9fd9cdcbb3ac77b447506
Peter Kidd <peter.kidd@cefas.gov.uk>
2023-10-10 18:21:19 +0100

    tidied up a bit

--- Functions/dataprep_funs.R
+++ Functions/dataprep_funs.R
@@ -1,52 +1,46 @@
-################################################################################
-######################## Prepare DATRAS Survey Data ############################
-################################################################################
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
 prepsurveydata <- function(hh, hl, ca, ices_rect){
-  tictoc::tic()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>> prepsurveydata >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+#> Basic cleaning of datras exchange data. Outputs a list of datasets.
+#> Includes hh,hl,ca, hlhh. hlhh dataset combines hl and hh data. 
+#> Also adds ICES Area_27 to dataset based on ICES rectangles. 
+#> Useful for filtering data to species stock division (e.g.ple.27.7d)
+#> ICES rectangles come from ICES shapefile.
+#> hlhh data is the datset used in most later funs. Other datasets kept for 
+#> for ease of access
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+
   message("Running unique() to get rid of duplicate rows")
   hh2 <- unique(hh)
   hl <- unique(hl)
   ca <- unique(ca)
-  tictoc::toc()
-  # we dont wat to do this for ca data 
-  # ca data can be duplicated if a species of the same length is sampled more than once
   
-  ### Subset DATRAS by Region
-  # We need to make the code so that we can subset the later maps and P-A 
-  # calculation by areas. To do this, we need to work out which rectangles belong 
-  # to which division and merge this with the DATRAS `HH` data. We will use the 
-  # ICES shapefiles and the ices data frames  created above
-  # record type.
-  tictoc::tic()
-  message("Getting location information from ICES shp")
-  
+  # Subset DATRAS by Region
+  # Which ICES rectangles belong to which ICES division 
+  message("Identifying Divisions for ICES Rectangles")
   area_div <- dplyr::distinct(ices_rect[c("ICESNAME", "Area_27", "Shape_Area")])
-  
+  # merge this with the DATRAS `HH` data. We will use the 
   hh <- merge.data.frame(hh2,     
                          area_div,
                          by.x = "StatRec",
                          by.y = "ICESNAME")
-  message("Checking merge")
-  ### check that the merge did not result in multiplication of rows
+  # check that the merge did not result in multiplication of rows
   if(!(nrow(hh) == nrow(hh2))){
     warning("Merge not correct. Number of rows between old and new dataset are not identical")
   }
   rm(hh2)
-  tictoc::toc()
   
-  # Create lon_lat to see unique combinations later
-  ## using Shoot
+  # Create lon_lat to see unique combinations using Shoot
   hh$lon_lat <- paste0(hh$ShootLong, "_", hh$ShootLat)
   
-  ## remove rows of data where TotalNo < 0
-  # identified this issue after funky Lorenz plots and TSS for D95 and Gini in turbot
-  # where many years of data had -9 TotalNo
-  # was there a reason that it was always -9??? (SNS survey)
+  # Remove rows of data where TotalNo < 0
+  # -9 is used as a placeholder for NA. Remove these.
   message("Removing rows in hl where TotalNo < 0")
   hl <- subset(hl, TotalNo >= 0)
   
-  tictoc::tic()
-  message("Merging hl and hh data into hlhh")
+  # HLHH
+  message("Creating hlhh data")
   # Create a Haul ID
   hh$haul.id <- as.character(paste(hh$Year, 
                                    hh$Quarter, 
@@ -65,39 +59,73 @@ prepsurveydata <- function(hh, hl, ca, ices_rect){
                                    hl$HaulNo, 
                                    sep = ":"))
   
-  ## merge location data from HH to HL species data
-  # check which columns are identical 
+  # Merge location data from HH to HL species data
+  ## check which columns are identical 
   cols <- janitor::compare_df_cols(hh, hl)
   na.omit(cols[cols$hh == cols$hl,])
+  ## Subset HH data to what we want
   m <- hh[c("haul.id", "Year", "Quarter", "Month", "Survey","Country", 
             "Ship", "Gear", "GearEx", "DoorType", "HaulDur", "HaulNo", 
             "StNo", "SweepLngt", "StatRec", "Area_27", "ShootLong", "ShootLat", "lon_lat", "HaulVal", "Depth")]
+  ## Merge	
   hlhh <- merge(hl,
                 dplyr::distinct(m),
                 c("haul.id", "Year", "Quarter", "HaulNo", "StNo", "Gear", "GearEx", "DoorType","Ship", 
                   "SweepLngt", "Country"))
   
-  message("Some data might be removed after merging hh and hl. This is typically due to haul.id's in HL not being in HH. Check which rows were removed, if any, by using the following code:")
-  print("anti_join(SURVEY_NAME.data$hl, SURVEY_NAME.data$hlhh, by = c('haul.id','Year','Quarter','Country','Ship','Gear','SweepLngt','GearEx','DoorType','StNo','HaulNo','TotalNo','HaulDur'))",
-        quote = FALSE)
+  #message("Some data might be removed after merging hh and hl. This is typically due to haul.id's in HL not being in HH. Check which rows were removed, if any, by using the following code:")
+  #print("anti_join(SURVEY_NAME.data$hl, SURVEY_NAME.data$hlhh, by = c('haul.id','Year','Quarter','Country','Ship','Gear','SweepLngt','GearEx','DoorType','StNo','HaulNo','TotalNo','HaulDur'))",
+  #      quote = FALSE)
   
   ## create list of datasets
   data.list <- list(hh = hh, 
                     hl = hl, 
                     ca = ca, 
                     hlhh = hlhh)
-  tictoc::toc()
   return(data.list)
 }
 
+?setdiff
 
-################################################################################
-############################## Spatial fortify #################################
-################################################################################
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+filtersurveydata <- function(hlhh, yrs, qrs, species_aphia, stk_divs){
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>> filtersurveydata >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+#> Filter data to the years, quarters, species, and stock divisions desired
+#> Resulting dataset can then be passed to spatial indicator functions in 
+#> spatinds_funs.R.
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
 
-## custom function to convert shapefiles to dataframe retaining all important information including coordinates
-# (function from Zach Radford)
+  yrs <- as.numeric(yrs)
+  qrs <- as.numeric(qrs)
+  ##Get all hauls sampled in the given years, quarters, and stock divisions
+  d <- hlhh %>%
+    filter(Area_27 %in% stk_divs,
+           Year %in% c(yrs),
+           Quarter %in% c(qrs),
+           HaulVal != "I") %>% # remove invalid hauls
+	distinct()
+  # Check years and quarters provided are available
+  if(!identical(as.numeric(unique(d$Quarter)), qrs)){
+    warning(paste0("Data not found in all survey quarters provided. 
+", "Only data for quarters ", paste(sort(unique(d$Quarter)), collapse = ", ")), " available in ", unique(hlhh$Survey.x), " survey data (years ", min(yrs), ":", max(yrs), "), region(s) ", stk_divs, ".
+", "No data for quarter ", paste(c(setdiff(qrs, unique(d$Quarter)), setdiff(unique(d$Quarter), qrs)), collapse = ", "), "\n", immediate. = TRUE)
+  }
+  if(!identical(as.numeric(sort(unique(d$Year))), yrs)){
+    warning(paste0("Species not found in all survey years provided. 
+", "No data for years ", paste(c(setdiff(yrs, unique(d$Year)), setdiff(unique(d$Year), yrs)), ", "), " in ", unique(hlhh$Survey.x), " survey data (quarters ", paste(sort(unique(d$Quarter)), collapse = ", "), "), region(s) ", stk_divs, ".\n"), immediate. = TRUE)
+  }
+ return(d)
+}
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
 makeReadableFortify <- function(shapefile) {
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Spatial fortify >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
+#> custom function to convert shapefiles to dataframe retaining all important 
+#> information including coordinates
+#>(function from Zach Radford)
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
   shapefile@data$id <- rownames(shapefile@data)
   shapefile.points <- fortify(shapefile)
   shapefile.df <- left_join(shapefile.points, shapefile@data, by = "id")
